4a587e6e05423c7218cb834e3e97769c2b53323e
Nathan Snoodyk <nathansnoodyk@gmail.com>
2024-03-11 13:54:04 +0000

    added secret key

--- photon/settings.py
+++ photon/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-sc=jlc_k32y!2(spdez3r16z4&@7h56-)4&s4x%gsg66w6nybg'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
