cf8014d413d19fe10287884e9c6cc4117565068a
Nathan Snoodyk <nathansnoodyk@gmail.com>
2024-03-13 14:57:58 +0000

    added whitenoise, static files

--- photon/settings.py
+++ photon/settings.py
@@ -16,7 +16,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
-ALLOWED_HOSTS = ['8000-snoodyk-snoodyk3dprints-bvv9b66q070.ws-eu108.gitpod.io','.herokuapp.com']
+ALLOWED_HOSTS = ['8000-snoodyk-snoodyk3dprints-bvv9b66q070.ws-eu110.gitpod.io','.herokuapp.com']
 
 
 # Application definition
@@ -34,6 +34,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -106,7 +107,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
