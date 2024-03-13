6cbf322e8b739875bc93ebfd8930bad9405e6a91
Nathan Snoodyk <nathansnoodyk@gmail.com>
2024-03-13 15:54:08 +0000

    changed template files

--- printblog/views.py
+++ printblog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.views import generic
 from .models import Post
 
@@ -7,3 +7,14 @@ class PostList(generic.ListView):
     queryset = Post.objects.filter(status=1)
     template_name = "printblog/index.html"
     paginate_by = 6
+
+def post_detail(request, slug):
+
+    queryset = Post.objects.filter(status=1)
+    post = get_object_or_404(queryset, slug=slug)
+
+    return render(
+        request,
+        "printblog/post_detail.html",
+        {"post": post},
+    )
